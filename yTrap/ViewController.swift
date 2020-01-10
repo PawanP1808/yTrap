a1e567428c7bc2f58d6d6f593008bf8fe4880770
Pawan <pawan@Pawans-MacBook-Pro.local>
2020-01-10 14:03:42 -0500

    fixed untracked files

---
+++ yTrap/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  yTrap
+//
+//  Created by Pawan on 2020-01-09.
+//  Copyright © 2020 SocialMusic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
